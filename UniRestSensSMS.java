e74fe01ca3ffcea99463314685b800c67fd4016e
Ajithaa Malini M <113791000+Ajithaa-Malini@users.noreply.github.com>
2023-04-08 14:42:07 +0530

    Add files via upload

--- UniRestSensSMS.java
+++ UniRestSensSMS.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package cyber;
 
 /**
  *
- * @author ponth
+ * @author Ajithaa Vanathi
  */
-public class UniRestSensSMS {
-    
+
+    import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
+public class UnirestSendSMS {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Sending SMS");
+
+            HttpResponse<String> response = Unirest.post("https://api.46elks.com/a1/SMS")
+                .basicAuth("<API Username>","<API Password>")
+                .field("to", "Any number")
+                .field("from", "Your phone number")
+                .field("message", "I am in danger!!!help me")
+                .asString();
+        
+            System.out.println(response.getBody());
+            }
+                
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
+
+
--- UnirestMakeACall.java
+++ UnirestMakeACall.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package cyber;
 
 /**
  *
- * @author ponth
+ * @author Ajithaa Vanathi
  */
-public class UnirestMakeACall {
-    
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
+public class UnirestMakeCall {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Sending SMS");
+
+            HttpResponse<String> response = Unirest.post("https://api.46elks.com/a1/Calls")
+                .basicAuth("<API Username>","<API Password>")
+                .field("to", "//Enter any valid number")
+                .field("from", "//Enter any valid number")
+                .field("voice_start", '{"connect":"//the number you have entered"}')
+                .asString();
+        
+            System.out.println(response.getBody());
+            }
+                
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
 }
